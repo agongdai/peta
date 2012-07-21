376fdd75c16712b93773cf89079d6d525bac68a6
Carl <caishaojiang@gmail.com>
2012-07-21 12:57:45 +0800

    preparing fastq files in merge.py

--- merge.py
+++ merge.py
@@ -1,12 +1,33 @@
 import sys, os
 from argparse import ArgumentParser
 
-def merge_fq(left_file, right_file):
+def rev_comp(sequence):
+    complement = {'A':'T','C':'G','G':'C','T':'A','N':'N'}
+    return "".join([complement.get(nt.upper(), '') for nt in sequence[::-1]])
+
+def combine(args):
+    files = args.inputs.split(',')
+    name = args.output
+    combined = open(name, 'w')
+    counter = 0
+    for f in files:
+        openf = open(f, 'r')
+        for line in openf:
+            if '@' in line:
+                combined.write('@' + str(counter) + '\n')
+                counter += 1
+            else:
+                combined.write(line)
+        openf.close()
+    combined.close()
+
+def merge_fq(args):
+    left_file = args.left
+    right_file = args.right
     try:
         left = open(left_file, 'r')
         right = open(right_file, 'r')
         filebase, ext = os.path.splitext(left_file)
-<<<<<<< HEAD
         combined_file = filebase.split('_')[0] + '.fastq'
         combined = open(combined_file, 'w')
         counter = 0
@@ -24,27 +45,14 @@ def merge_fq(left_file, right_file):
                 combined.write(l)
                 counter += 1
                 
-=======
-        combined_file = filebase.split('_')[0]
-        combined = open(combined_file, 'w')
-        counter = 0
-        seqs = []
-        for line in left:
-            if '@' in line:
-                combined.write('@' + str(counter) + '\n')
->>>>>>> b9788344a7b6beacf56f48db6fb3f258dfe3df2d
                 r = right.readline()
                 combined.write('@' + str(counter) + '\n')
                 r = right.readline()
+                r = rev_comp(r) + '\n'
                 combined.write(r)
                 r = right.readline()
                 combined.write(r)
                 r = right.readline()
-<<<<<<< HEAD
-=======
-                counter += 1
-                combined.write('@' + str(counter) + '\n')
->>>>>>> b9788344a7b6beacf56f48db6fb3f258dfe3df2d
                 combined.write(r)
                 counter += 1
             line = left.readline()
@@ -57,14 +65,18 @@ def merge_fq(left_file, right_file):
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('read_file_1', help='Left mates file')
-    parser.add_argument('read_file_2', help='Right mates file')
+    subparsers = parser.add_subparsers(help='sub command help')
+    parser_combine = subparsers.add_parser('combine', help='combine multiple files')
+    parser_combine.set_defaults(func=combine)
+    parser_combine.add_argument('-i', required=True, help='the files to combine, seperated by a ","', dest='inputs')
+    parser_combine.add_argument('-o', required=True, help='combined file name', dest='output')
+
+    parser_merge = subparsers.add_parser('merge', help='merge the shuffled fasta/fastq files, reverse complement the right mates')
+    parser_merge.set_defaults(func=merge_fq)
+    parser_merge.add_argument('-l', required=True, help='left mate file', dest='left')
+    parser_merge.add_argument('-r', required=True, help='right mate file', dest='right')
     args = parser.parse_args()
-<<<<<<< HEAD
-    merge_fq(args.read_file_1, args.read_file_2)
-=======
-    merge(args.read_file_1, args.read_file_2)
->>>>>>> b9788344a7b6beacf56f48db6fb3f258dfe3df2d
-    
+    args.func(args)
+
 if __name__ == '__main__':
     sys.exit(main())
