438ab0318f8efbeb0ab557bad4116ee22685b9ea
Carl <caishaojiang@gmail.com>
2012-11-09 14:11:46 +0800

    Testing the scripts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,4 @@
-query_dir=rnaseq/Spombe/SRR097897
+query_dir=../rnaseq/Spombe/SRR097897
 
 ./peta clean -k 15 -l $query_dir/SRR097897 $query_dir/SRR097897.fa
 
--- scripts/pair.sh
+++ scripts/pair.sh
@@ -1,3 +1,3 @@
-query_dir=rnaseq/Spombe/SRR097897
+query_dir=../rnaseq/Spombe/SRR097897
 ./peta pair -p 100000 -o 33 $query_dir/SRR097897.fa $query_dir/SRR097897.solid 
 
