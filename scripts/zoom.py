cae91fb364032e6892da3295f456d0164997ef90
Carl <caishaojiang@gmail.com>
2013-01-29 00:36:17 +0800

    Done drawing dot files of annotated transcripts

--- scripts/zoom.py
+++ scripts/zoom.py
@@ -163,6 +163,26 @@ def get_align_str(ref, fasta, hits):
         read_no += 1
     return algin_str
 
+class Block(object):
+    def __init__(self, tx, id, start, end, is_covered, matched_block=None):
+        self.tx = tx
+        self.id = id
+        self.start = start
+        self.end = end
+        self.is_covered = is_covered
+        self.matched_block = matched_block
+    def __repr__(self):
+        s = '%s, %d: %d~%d, %r' % (self.tx, self.id, self.start, self.end, self.is_covered)
+        return s
+
+def get_color(transcript, tx, b):
+    if not b.is_covered:
+        return 'gray'
+    if tx == transcript:
+        return 'yellow'
+    else:
+        return 'white'
+
 def draw_dot(args):
     tx = FastaFile(args.tx)
     dot = open(args.transcript + '.dot', 'w')
@@ -188,105 +208,138 @@ def draw_dot(args):
                     m.append(h.q_block_starts[i] + h.block_sizes[i])
                      
     blocks = {}
+    tx_cursors = {}
+    tx_node_ids = {}
     milestones.sort()
     blocks[args.transcript] = []
+    tx_node_ids[args.transcript] = 0
+    tx_cursors[args.transcript] = 0
     dot.write('digraph g { \n\trankdir = LR \n')
+    n_solid_blocks = 0
     # Determine all blocks of the query transcript
     for i in range(len(milestones) - 1):
         (next_m, next_start_or_end, next_index) = milestones[i + 1]
         (m, start_or_end, index) = milestones[i]
         is_covered = True
         if i == 0 and m > 0:
-            blocks[args.transcript].append((0, m, False))
+            b = Block(args.transcript, -1, 0, m, False)
+            blocks[args.transcript].append(b)
         if start_or_end == 'end' and next_start_or_end == 'start' and index < next_index:
             is_covered = False
-        blocks[args.transcript].append((m, next_m, is_covered))
-        print i, len(milestones)
-        print milestones[i + 1], tx.get_seq_len(args.transcript)
+        if is_covered:
+            b = Block(args.transcript, n_solid_blocks, m, next_m, is_covered)
+            b.matched_block = b
+            n_solid_blocks += 1
+        else:
+            b = Block(args.transcript, -1, m, next_m, is_covered)
+        blocks[args.transcript].append(b)
+        #print i, len(milestones)
+        #print milestones[i + 1], tx.get_seq_len(args.transcript)
         if i == len(milestones) - 2 and next_m < tx.get_seq_len(args.transcript):
-            blocks[args.transcript].append((next_m, h.qlen, False))
+            b = Block(args.transcript, -1, next_m, h.qlen, False)
+            blocks[args.transcript].append(b)
 
     # Determine the blocks of 'reference' transcripts
-    n_solid_blocks = 0
     for h in hits:
         rname = h.rname
         if h.qname == args.transcript and not h.qname == h.rname:
             blocks[rname] = []
+            tx_cursors[rname] = 0
+            tx_node_ids[rname] = 0
             for i in range(h.n_blocks):
                 if i == 0:
                     if h.r_block_starts[0] > 0:
-                        blocks[rname].append((0, h.r_block_starts[0], False))
+                        b = Block(rname, len(blocks[rname]), 0, h.r_block_starts[0], False)
+                        blocks[rname].append(b)
                 q_start = h.q_block_starts[i]
                 q_end = q_start + h.block_sizes[i]
                 # For those blocks covered by this transcript, add them
-                for (m, next_m, is_covered) in blocks[args.transcript]:
+                for j in range(len(blocks[args.transcript])):
+                    b = blocks[args.transcript][j]
                     diff = h.r_block_starts[i] - h.q_block_starts[i]
-                    if (m >= q_start and next_m <= q_end):
-                        blocks[rname].append((m + diff, next_m + diff, True))
-                        n_solid_blocks += 1
-                    if next_m > q_end:
+                    if (b.start >= q_start and b.end <= q_end):
+                        new_b = Block(rname, b.id, b.start + diff, b.end + diff, True, b)
+                        blocks[rname].append(new_b)
+                    if b.end >= q_end:
                         break
                 # Add the 'not hit' block in between the hit blocks
-                if i < h.n_blocks - 1:
-                    blocks[rname].append((h.r_block_starts[i] + h.block_sizes[i], h.r_block_starts[i + 1], False))
+                if i < h.n_blocks - 1 and h.r_block_starts[i] + h.block_sizes[i] < h.r_block_starts[i + 1]:
+                    b = Block(rname, len(blocks[rname]), h.r_block_starts[i] + h.block_sizes[i], h.r_block_starts[i + 1], False)
+                    blocks[rname].append(b)
                 # If all blocks do not cover till the end, add the remaining area as a 'not hit' block
                 if i == h.n_blocks - 1 and h.block_sizes[i] + h.r_block_starts[i] < h.rlen:
-                    blocks[rname].append((h.block_sizes[i] + h.r_block_starts[i], h.rlen, False))
+                    b = Block(rname, len(blocks[rname]), h.block_sizes[i] + h.r_block_starts[i], h.rlen, False)
+                    blocks[rname].append(b)
     
     for t, tx_blocks in blocks.iteritems():
+        print t, tx_blocks
         if len(blocks[t]) > 0:
-            (m, next_m, is_covered) = blocks[t][j]
             dot.write('\t%s_0 [fixedsize=true, width=3, style=filled, fillcolor=%s, shape=box, label="%s:%d"] \n' % (t, 'orange', t, tx.get_seq_len(t)))
     
-    has_dummy = False
-    for t, tx_blocks in blocks.iteritems():
-        if len(blocks[t]) > 0:
-            (m, next_m, is_covered) = blocks[t][j]
-            if is_covered:
-                has_dummy = True
-                break
-
-    n_has_drawn = 1
-    for t, tx_blocks in blocks.iteritems():
-        color = 'white'
-        if t == args.transcript:
-            color = 'yellow'
-        if len(blocks[t]) > 0:
-            (m, next_m, is_covered) = blocks[t][0]
-            if has_dummy:
-                if not is_covered:
-                    dot.write('\t%s_%d [fixedsize=true, width=2, style=filled, fillcolor=%s, shape=box, label="%s:%d"] \n' % (t, n_has_drawn, color, t, tx.get_seq_len(t)))
-                    dot.write('\t%s_%d -> %s_%d \n' % (t, n_has_drawn - 1, t, n_has_drawn))
+    print 'n_solid_blocks ', n_solid_blocks
     
-    for t, tx_blocks in blocks.iteritems():
-        if t == args.transcript:
-            continue        
-        for i in range(len(blocks[t])):
-            (m, next_m, is_covered) = blocks[t][i]
-            color = 'white'
-            if not is_covered:
-                color = 'gray'
-            if i == 0:
-                dot.write('\t%s [fixedsize=true, width=3, style=filled, fillcolor=%s, shape=box, label="%s:%d"] \n' % (t, 'orange', t, tx.get_seq_len(t)))
-                dot.write('\t%s_%d [fixedsize=true, width=2, style=filled, fillcolor=%s, shape=box, label="%d: %d~%d"] \n' % (t, i, color, next_m - m, m, next_m))
-                dot.write('\t%s -> %s_%d \n' % (t, t, 0))
+    for i in range(n_solid_blocks + 1):
+        # Check whether there is a 'not hit' block before hit block i
+        has_dummy = False
+        all_covered = True
+        
+        for t, tx_blocks in blocks.iteritems():
+            if tx_cursors[t] < len(tx_blocks):
+                b = tx_blocks[tx_cursors[t]]
+                matched_b = b.matched_block
+                if not b.is_covered:
+                    has_dummy = True
+                    all_covered = False
+                if b.id != i:
+                    has_dummy = True
+        
+        print '----------------------'    
+        print 'has_dummy ', has_dummy
+        print 'all_covered ', all_covered
+        print tx_cursors
+        # For each 'referece transcript', draw until the current 'hit block' i
+        for t, tx_blocks in blocks.iteritems():
+            cursor = tx_cursors[t]
+            if cursor >= len(tx_blocks):
+                continue
+            b = tx_blocks[cursor]
+            if has_dummy:
+                if b.is_covered:
+                    if not all_covered:
+                        dot.write('\t%s_%d [fixedsize=true, width=2, style="filled,dashed", fillcolor=%s, shape=box, label=""] \n' % (t, tx_node_ids[t] + 1, get_color(args.transcript, t, b)))
+                        dot.write('\t%s_%d -> %s_%d \n' % (t, tx_node_ids[t], t, tx_node_ids[t] + 1))
+                        tx_node_ids[t] += 1
+                    if b.id == i:
+                        dot.write('\t%s_%d [fixedsize=true, width=2, style="filled", fillcolor=%s, shape=box, label="%d: %d~%d"] \n' % (t, tx_node_ids[t] + 1, get_color(args.transcript, t, b), b.end - b.start, b.start, b.end))
+                        dot.write('\t%s_%d -> %s_%d \n' % (t, tx_node_ids[t], t, tx_node_ids[t] + 1))
+                        tx_cursors[t] += 1
+                        tx_node_ids[t] += 1
+                    else:
+                        dot.write('\t%s_%d [fixedsize=true, width=2, style="filled,dashed", fillcolor=%s, shape=box, label=""] \n' % (t, tx_node_ids[t] + 1, get_color(args.transcript, t, b)))
+                        dot.write('\t%s_%d -> %s_%d \n' % (t, tx_node_ids[t], t, tx_node_ids[t] + 1))
+                        tx_node_ids[t] += 1
+                else:
+                    dot.write('\t%s_%d [fixedsize=true, width=2, style="filled", fillcolor=%s, shape=box, label="%d: %d~%d"] \n' % (t, tx_node_ids[t] + 1, get_color(args.transcript, t, b), b.end - b.start, b.start, b.end))
+                    dot.write('\t%s_%d -> %s_%d \n' % (t, tx_node_ids[t], t, tx_node_ids[t] + 1))
+                    tx_cursors[t] += 1
+                    tx_node_ids[t] += 1
+                    if tx_cursors[t] < len(tx_blocks):
+                        b = tx_blocks[tx_cursors[t]]
+                        if b.is_covered and b.id == i:
+                            dot.write('\t%s_%d [fixedsize=true, width=2, style="filled", fillcolor=%s, shape=box, label="%d: %d~%d"] \n' % (t, tx_node_ids[t] + 1, get_color(args.transcript, t, b), b.end - b.start, b.start, b.end))
+                            dot.write('\t%s_%d -> %s_%d \n' % (t, tx_node_ids[t], t, tx_node_ids[t] + 1))
+                            tx_cursors[t] += 1
+                            tx_node_ids[t] += 1
+                        else:
+                            dot.write('\t%s_%d [fixedsize=true, width=2, style="filled,dashed", fillcolor=%s, shape=box, label=""] \n' % (t, tx_node_ids[t] + 1, get_color(args.transcript, t, b)))
+                            dot.write('\t%s_%d -> %s_%d \n' % (t, tx_node_ids[t], t, tx_node_ids[t] + 1))
+                            tx_node_ids[t] += 1
             else:
-                dot.write('\t%s_%d [fixedsize=true, width=2, style=filled, fillcolor=%s, shape=box, label="%d: %d~%d"] \n' % (t, i, color, next_m - m, m, next_m))
-                dot.write('\t%s_%d -> %s_%d \n' % (t, i - 1, t, i))
-                
-    for i in range(len(blocks[args.transcript])):
-        (m, next_m, is_covered) = blocks[args.transcript][i]
-        color = 'yellow'
-        if not is_covered:
-            color = 'gray'
-        if i == 0:
-            dot.write('\t%s [fixedsize=true, width=3, style=filled, fillcolor=%s, shape=box, label="%s:%d"] \n' % (args.transcript, 'orange', args.transcript, tx.get_seq_len(args.transcript)))
-            dot.write('\t%s_%d [fixedsize=true, width=2, style=filled, fillcolor=%s, shape=box, label="%d: %d~%d"] \n' % (args.transcript, i, color, next_m - m, m, next_m))
-            dot.write('\t%s -> %s_%d \n' % (args.transcript, args.transcript, 0))
-        else:
-            dot.write('\t%s_%d [fixedsize=true, width=2, style=filled, fillcolor=%s, shape=box, label="%d: %d~%d"] \n' % (args.transcript, i, color, next_m - m, m, next_m))
-            dot.write('\t%s_%d -> %s_%d \n' % (args.transcript, i - 1, args.transcript, i))
-
+                dot.write('\t%s_%d [fixedsize=true, width=2, style="filled", fillcolor=%s, shape=box, label="%d: %d~%d"] \n' % (t, tx_node_ids[t] + 1, get_color(args.transcript, t, b), b.end - b.start, b.start, b.end))
+                dot.write('\t%s_%d -> %s_%d \n' % (t, tx_node_ids[t], t, tx_node_ids[t] + 1))
+                tx_cursors[t] += 1
+                tx_node_ids[t] += 1
+        
     dot.write('} \n')
     dot.close()
 
