1ea19be2a9ded4ad277424272bba731318c42f1b
Carl <caishaojiang@gmail.com>
2013-04-21 20:31:02 +0800

    Pseudo-branching sent to Prof.

--- scripts/Pseudo-branching.c
+++ scripts/Pseudo-branching.c
@@ -8,9 +8,11 @@ Function kmer_ext:
 	ordered_kmer = [list of kmers with frequency >1, ordered by frequency decreasingly]
 	hash_table = {the kmer hash table built before: kmer_integer->[frequency, read_1, read_2...] }
 	foreach kmer in ordered_kmer:
-		new_template = kmer									// Start from the kmer
-		template_ext(new_template, hash_table, kmer, 0)		// Extend to the right
-		template_ext(new_template, hash_table, kmer, 1)		// Extend to the left
+		new_template = kmer											// Start from the kmer
+		template_ext(new_template, hash_table, kmer, 0)				// Extend to the right
+		template_ext(new_template, hash_table, kmer, 1)				// Extend to the left
+		update_branching_events(BRANCHING_EVENTS, new_template)		// The locus info needs to update
+		update_ambiguous_bases(AMBIGUOUS_BASES, new_template)		// The locus info needs to update
 		ALL_TEMPLATES.add(template)
 
 /**
@@ -18,8 +20,8 @@ Function kmer_ext:
 	Main process (together with Function kmer_ext):
 		1. Start from some kmer;
 		2. Extend the template to the right, based on frequencies of next probable kmers;
-		3. If next kmers is ambiguous, and there are junction reads supporting branches:
-			3.1 extend the current template (repeat step 2)
+		3. If next kmer is ambiguous, and there are junction reads supporting branching:
+			3.1 extend the main template (repeat step 2)
 			3.2 extend the branch template (repeat step 2), to one direction only:
 			3.3 record the branching event to BRANCHING_EVENTS
 			3.4 add the branch template to ALL_TEMPLATES
@@ -39,34 +41,38 @@ Function template_ext(template, hash_table, query, direction):
 		if (second_c != -1):
 			// If it is just noise, likely to come to the same sequence quickly
 			// So here try to extend extra 5 bases quickly, for both branches
-			max_query = shift_query(query, max_c, direction)
-			max_branch = try_short_tpl_ext(max_query, direction, 5)
-			branch_query = shift_query(query, second_c, direction)
-			second_branch = try_short_tpl_ext(branch_query, direction, 5)
+			main_query = shift_query(query, max_c, direction)
+			main_branch = try_short_tpl_ext(main_query, direction, 5)
+			second_query = shift_query(query, second_c, direction)
+			second_branch = try_short_tpl_ext(second_query, direction, 5)
 
 			// If there is probable branching
-			if (max_branch != second_branch): 
-				second_branch = try_short_tpl_ext(branch_query, direction, hash_table.read_length - 5)
-				has_junction_read = find_junction_reads(template, second_branch)
+			if (main_branch != second_branch): 
+				second_branch = try_short_tpl_ext(second_query, direction, hash_table.read_length - 5)
+				has_junction_read = find_junction_reads(template, second_branch, hash_table, direction)
 				// If the second branch is supported by some junction reads, extend it
 				if (has_junction_read):
 					print "Extending the main template first"
 					branching_locus = template.length
 					// Not using the result of try_short_tpl_ext, because their kmers are not marked as used.
 					shift_template(template, c, direction)
-					template_ext(template, hash_table, max_query, direction)
+					// Recursivly call this function itself to extend
+					template_ext(template, hash_table, main_query, direction)
 					print "Extending the branch at %d" % branching_locus
-					// For the branch, extend to only one direction, because another direction is extended by main template already
+					// For the second branch, extend to only one direction, because another direction is extended by main template already
+					// Not using the result of try_short_tpl_ext, because their kmers are not marked as used.
 					second_branch = second_c
-					template_ext(second_branch, hash_table, branch_query, direction)
+					// In case of branching on short templates
+					second_branch.virtual_tail = cut_template_tail(template, hash_table.read_length - 5, direction)
+					template_ext(second_branch, hash_table, second_query, direction)
 					branching_points.add((template, second_branch, branching_locus, direction))
 					ALL_TEMPLATES.add(second_branch)
 					// Jump out of the function
 					return
 			else:
-				// It is just an ambiguous base because of sequencing errors, etc.
+				// It is just an ambiguous base because of sequencing errors, etc. Record it, continue extension
 				mark_kmer_used(hash_table, branch_query)
-				ambiguous_bases.add((template, template.length))
+				AMBIGUOUS_BASES.add((template, template.length))
 		else:
 			// For every kmer used during this extension, mark the frequency as 0.
 			mark_kmer_used(hash_table, query)
@@ -87,7 +93,7 @@ Function try_short_tpl_ext(query, direction, max_len):
 		if (c == -1):
 			break;
 		else:
-			extend_template(branch_template, c, direction)
+			shift_template(branch_template, c, direction)
 			query_copy = shift_query(query_copy, c, direction)
 	return branch_template	
 
@@ -99,14 +105,14 @@ Function try_short_tpl_ext(query, direction, max_len):
 			main_template:     ATC...CCCCCCCCCCCCCCC (length 1000)
 			branch:                                 TTTTTTTTTT (length 15-5=10)
 			junction_template:            CCCCCCCCCCTTTTTTTTTT (length 15*2-5*2=20)
-			junction_reads:               CCCCCCCCCCTTTTT
-			                                 CCCCCCCTTTTTTTT 
+			junction_reads:                CCCCCCCCCTTTTTT
+			                                  CCCCCCTTTTTTTTT 
 **/
-Function find_junction_reads(main_template, branch, hash_table):
+Function find_junction_reads(main_template, branch, hash_table, direction):
 	// Get the tail of the main template
-	main_template_tail = get_tail(main_template, hash_table.read_length - 5)
+	main_template_tail = cut_template_tail(main_template, hash_table.read_length - 5, direction)
 	// Concat the two branches, temporarily
-	probable_junction_template = concat(main_template, branch)
+	probable_junction_template = concat(main_template_tail, branch)
 	junction_reads = []
 	for (i = 0; i < probable_junction_template.length - hash_table.read_length; i++):
 		// query length is same as read length, allowing 1 mismatch
@@ -115,6 +121,20 @@ Function find_junction_reads(main_template, branch, hash_table):
 	if (junction_reads.length > 0):
 		return 1
 	return 0
+
+
+// ========= Supportive functions ==========
+
+/**
+	Get the tail of a template. 
+	If the length is not long enough, try to get from the 'virtual_tail'
+**/
+Function cut_template_tail(template, tail_length, direction):
+	tail = NULL
+	tpl_copy = template
+	if (tpl_copy.length < tail_length):
+		tpl_copy = concat(template.virtual_tail, tpl_copy)
+	return simple_tail(tpl_copy, tail_length, direction)
 
 /**
 	Start from locus i on the template, return the int64 of the new query with certain length
@@ -151,6 +171,7 @@ Function get_max_index(counters):
 		return -1
 	return max_index
 
+
 // ========= Operations on the hash table ==========
 
 /**
@@ -160,8 +181,8 @@ Function get_max_index(counters):
 			frequencies: 	 ATCTA: 10
 							 ATCTC: 2
 							 ATCTG: 0
-							 ATCTT: 0
-			to return: 		[10, 2, 0, 0]
+							 ATCTT: 1
+			to return: 		[10, 2, 0, 1]
 **/
 Function count_next_kmers(hash_table, query, direction):
 	counters = []
