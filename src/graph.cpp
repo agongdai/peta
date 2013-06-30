f08d8315f0c712f4563ff528cd71cf32cd243605
Cai Shaojiang <caishaojiang@gmail.com>
2013-06-30 17:08:16 +0800

    Added graph.cpp/hpp

---
+++ src/graph.cpp
@@ -0,0 +1,26 @@
+/*
+ * graph.cpp
+ *
+ *  Created on: Jun 30, 2013
+ *      Author: carl
+ */
+
+#include <unordered_map>
+#include <stdint.h>
+#include <glib.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+#include "bwtaln.h"
+#include "edge.h"
+#include "kmers.hpp"
+#include "ass.hpp"
+#include "rnaseq.h"
+#include "pechar.h"
+#include "utils.h"
+#include "peseq.h"
+#include "junction.hpp"
+
+
---
+++ src/graph.hpp
@@ -0,0 +1,13 @@
+/*
+ * graph.hpp
+ *
+ *  Created on: Jun 30, 2013
+ *      Author: carl
+ */
+
+#ifndef GRAPH_HPP_
+#define GRAPH_HPP_
+
+
+
+#endif /* GRAPH_HPP_ */
